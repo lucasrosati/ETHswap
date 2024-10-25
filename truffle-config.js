8647779877c7313a131d4667c70270d273c152e2
lucasrosati <lucasrosati@hotmail.com>
2024-10-25 16:13:30 -0300

    Commit inicial do projeto EthSwap

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,16 @@
-require('babel-register');
-require('babel-polyfill');
-
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1",     // Localhost (ganache)
+      port: 7545,            // Porta padrão do Ganache
+      network_id: "*",       // Qualquer rede
     },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
+      version: "^0.5.0",
     }
   }
 }
